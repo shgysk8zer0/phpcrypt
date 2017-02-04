59f5079e755e4b7ccc590353b31a46a2a6b3d474
Chris Zuber <shgysk8zer0@gmail.com>
2017-02-04 13:28:14 -0800

    Update testing script
    - Now tests hashing as well.
    - Check that `PHP_SAPI` is "cli" or "cli-server"
    - All printing now done in JSON

--- index.php
+++ index.php
@@ -7,6 +7,14 @@ const PRIVATE_KEY = 'private.pem';
 const MESSAGE     = 'Hello world!';
 const ERROR_LOG   = 'errors.log';
 
+if (! in_array(PHP_SAPI, ['cli', 'cli-server'])) {
+	http_response_code(503);
+	exit();
+} elseif (version_compare(\PHP_VERSION, '7.0.0', '<')) {
+	echo 'PHP version 7 or greater is required.' . PHP_EOL;
+	exit(1);
+}
+
 set_include_path(dirname(__DIR__, 2) . PATH_SEPARATOR . __DIR__);
 spl_autoload_register('spl_autoload');
 
@@ -39,7 +47,12 @@ function error_handler(
 function exception_handler(\Throwable $exc)
 {
 	error_log($exc . PHP_EOL, 3, ERROR_LOG);
-	echo $exc . PHP_EOL;
+	echo json_encode([
+		'message' => $exc->getMessage(),
+		'file'    => $exc->getFile(),
+		'line'    => $exc->getLine(),
+		'trace'   => $exc->getTrace(),
+	], JSON_PRETTY_PRINT) . PHP_EOL;
 	exit(1);
 }
 
@@ -52,23 +65,37 @@ $pair->private->exportToFile(PRIVATE_KEY, PASSWORD);
 
 $keys = new KeyPair(PUBLIC_KEY, PRIVATE_KEY, PASSWORD);
 
-$encrypted = $keys->encrypt(MESSAGE);
-$decrypted = $keys->decrypt($encrypted);
-$sig       = $keys->sign($encrypted);
-$valid     = $keys->verify($encrypted, $sig);
+$encrypted  = $keys->encrypt(MESSAGE);
+$decrypted  = $keys->decrypt($encrypted);
+$sig        = $keys->sign($encrypted);
+$valid      = $keys->verify($encrypted, $sig);
+$hash       = Hash::sha384(MESSAGE);
+$matches    = Hash::match(MESSAGE, $hash);
+$file_hash  = Hash::sha512File(__FILE__);
+$file_match = Hash::matchFile(__FILE__, $file_hash);
 
-print_r([
-	'keys'      => "$keys",
-	'message'   => MESSAGE,
-	'encrypted' => $encrypted,
-	'decrypted' => $decrypted,
-	'match'     => MESSAGE === $decrypted,
-	'signature' => $sig,
-	'valid'     => $valid,
-]);
+
+@header('Content-Type: application/json');
+echo json_encode([
+	'keys'       => "$keys",
+	'message'    => MESSAGE,
+	'encrypted'  => $encrypted,
+	'decrypted'  => $decrypted,
+	'match'      => MESSAGE === $decrypted,
+	'signature'  => $sig,
+	'valid'      => $valid,
+	'hash'       => $hash,
+	'hash match' => $matches,
+	'file hash'  => $file_hash,
+	'file match' => $file_match,
+], JSON_PRETTY_PRINT);
 
 if (!$decrypted === MESSAGE) {
 	trigger_error('Decrypted message does not match original.');
 } elseif (!$valid) {
 	trigger_error('Signature not valid.');
+} elseif (!$matches) {
+	trigger_error('Hash does not match');
+} elseif (!$file_match) {
+	trigger_error(sprintf('Hash for %s did not match', __FILE__));
 }
